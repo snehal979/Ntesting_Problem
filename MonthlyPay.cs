9548de0c6221b166d99e4dd47f2e75ad1322a64a
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-25 14:20:00 +0530

    Add files via upload

---
+++ MonthlyPay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntesting
+{
+    public class MonthlyPay
+    {
+        public static void PaymentOfMonth(int P, int R, int Y)
+        {
+            //Y years to pay off a P principal loan amount at R percent interest compounded monthly.
+            // where r= R/(12*100) and n = 12*Y
+            float r = (float)R / (12 * 100);
+            int n = 12 * Y;
+
+            double payment = (P * r) /(1 - (Math.Pow((1 + r), -n)));
+            Console.WriteLine("Monthly payment you have to make for year "+Y +" payment is " +payment);
+
+        }
+
+    }
+}
---
+++ VendingMachine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntesting
+{
+    internal class VendingMachine
+    {
+        int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+        int[] currency = new int[8];
+        int count = 0;
+        public void ChangeAmmount(int amount)
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount / notes[i] >= 1)
+                {
+                    currency[count] = notes[i];
+                    count++;
+                    amount -= notes[i];
+                    if (amount != 0)
+                    {
+                        this.ChangeAmmount(amount);
+                    }
+                    else
+                        return;
+                    break;
+                }
+                else
+                    continue;
+            }
+            if (count>0)
+                Console.WriteLine("Number of notes required is: "+count);
+        }
+        public void Print()
+        {
+            int back = 0;
+            for (int i = 0; i < currency.Length; i++)
+            {
+                int count = 0;
+                if (currency[i] !=0)
+                {
+                    foreach (var data in currency)
+                    {
+                        if (currency[i] == data && data != back)
+                        {
+                            count++;
+                        }
+                    }
+                    back = currency[i];
+                    if (count> 0)
+                    {
+                        Console.WriteLine(count + ":" + currency[i]);
+                    }
+                }
+            }
+
+        }
+    }
+}
+
